5f8c8df0e2043efbd28d20b5858b618bbbafd7a8
Rob <rjl6789@hotmail.co.uk>
2019-05-27 23:21:11 +0100

    minor tidy

--- dwm.c
+++ dwm.c
@@ -801,7 +801,7 @@ drawbar(Monitor *m)
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
-	if ((w = m->ww - sw - x ) > bh) {
+	if ((w = m->ww - sw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
