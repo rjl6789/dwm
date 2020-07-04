b47b23596379624ff9dff702309294242e515b84
rjl6789 <rlives6789@gmail.com>
2020-07-04 13:42:56 +0100

    gruvbox theme and colorbar patch

--- config.h
+++ config.h
@@ -1,24 +1,49 @@
 /* See LICENSE file for copyright and license details. */
-
+#include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const int gappx     = 5;                 /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Mono:size=12",
-	"JoyPixels:pixelsize=10:antialias=true:autohint=true",
-	"Font Awesome 5 Free Solid:size=10"};
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = {
+	"monospace:size=11",
+	"JoyPixels:pixelsize=14:antialias=true:autohint=true",
+};
+static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_cursor[]      = "#d65d0e";
+static const char col_fg[]          = "#ebdbb2";
+static const char col_bg[]          = "#282828";
+static const char col_0[]           = "#1d2021";
+static const char col_1[]           = "#cc241d";
+static const char col_2[]           = "#98971a";
+static const char col_3[]           = "#d79921";
+static const char col_4[]           = "#458588";
+static const char col_5[]           = "#b16286";
+static const char col_6[]           = "#689d6a";
+static const char col_7[]           = "#a89984";
+static const char col_8[]           = "#928374";
+static const char col_9[]           = "#fb4934";
+static const char col_10[]          = "#b8bb26";
+static const char col_11[]          = "#fabd2f";
+static const char col_12[]          = "#83a598";
+static const char col_13[]          = "#d3869b";
+static const char col_14[]          = "#8ec07c";
+static const char col_15[]          = "#ebdbb2";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*                   fg         bg          border   */
+	[SchemeNorm]     = { col_fg,    col_bg,     col_8 },
+	[SchemeSel]      = { col_bg,    col_cursor, col_cursor },
+	[SchemeStatus]   = { col_fg,    col_bg,     col_8 },
+	[SchemeTagsNorm] = { col_fg,    col_bg,     col_8 },
+	[SchemeTagsSel]  = { col_bg,    col_cursor, col_cursor },
+	[SchemeInfoNorm] = { col_fg,    col_bg,     col_8 },
+	[SchemeInfoSel]  = { col_bg,    col_4,      col_4 },
 };
 
 /* tagging */
@@ -51,7 +76,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -63,7 +88,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_cursor, "-sf", col_bg, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 #include "movestack.c"
@@ -111,6 +136,22 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0, XF86XK_AudioMute,		spawn,		SHCMD("volumeControl togmute; kill -44 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioRaiseVolume,	spawn,		SHCMD("volumeControl up; kill -44 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioLowerVolume,	spawn,		SHCMD("volumeControl down; kill -44 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioPrev,		spawn,		SHCMD("mpc prev") },
+	{ 0, XF86XK_AudioNext,		spawn,		SHCMD("mpc next") },
+	{ 0, XF86XK_AudioPause,		spawn,		SHCMD("mpc pause") },
+	{ 0, XF86XK_AudioPlay,		spawn,		SHCMD("mpc play") },
+	{ 0, XF86XK_AudioStop,		spawn,		SHCMD("mpc stop") },
+	{ 0, XF86XK_AudioRewind,	spawn,		SHCMD("mpc seek -10") },
+	{ 0, XF86XK_AudioForward,	spawn,		SHCMD("mpc seek +10") },
+	{ 0, XF86XK_AudioMedia,		spawn,		SHCMD("alacritty -e ncmpcpp") },
+	{ 0, XF86XK_Calculator,		spawn,		SHCMD("alacritty -e bc -l") },
+	{ 0, XF86XK_WWW,		spawn,		SHCMD("$BROWSER") },
+	{ 0, XF86XK_Mail,		spawn,		SHCMD("alacritty -e neomutt ; pkill -RTMIN+12 dwmblocks") },
+	{ 0, XF86XK_MonBrightnessUp,	spawn,		SHCMD("brightnessControl up") },
+	{ 0, XF86XK_MonBrightnessDown,	spawn,		SHCMD("brightnessControl down") },
 };
 
 /* button definitions */
